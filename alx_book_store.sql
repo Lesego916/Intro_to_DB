36e0848fede395334851770bb14f53a53460082a
Lesego916 <segobuyisile@gmail.com>
2025-10-01 15:34:30 +0200

    BE

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -28,22 +28,23 @@ CREATE TABLE IF NOT EXISTS customers (
     address TEXT
 );
 
--- Orders Table
-CREATE TABLE IF NOT EXISTS orders (
+-- Orders table
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Order_Details Table
-CREATE TABLE IF NOT EXISTS order_details (
+-- Order_Details table
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
+
 
 
